a6f710b28bae04f199e6202e6592f5a26852793d
Juanda <juandac98@outlook.com>
2026-04-10 10:46:35 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        int numero = 10;
+        int [] array = new int[] {1, 2, 3, 4, 5};
+        System.out.println(numero);
+        System.out.println(array);
+        for (int i=0; i < array.length; i++) { // for each (desde;mientras;incremento)
+            System.out.println(array[i]);
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
